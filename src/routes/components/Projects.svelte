b4dcab04b022def57891b8949be0dc3bbffd523c
tristan <heytristaann@gmail.com>
2022-10-13 19:46:43 -0400

    fixed projects header

--- src/routes/components/Projects.svelte
+++ src/routes/components/Projects.svelte
@@ -32,8 +32,8 @@
     }}
 >
     <div class="group">
-        <a class="text-white text-4xl font-black tracking-widest duration-500 ease-in-out" rel="noopener noreferrer" target="_blank" href="https://github.com/realTristan?tab=repositories">PROJECTS</a>
-        <div class="bg-[#1014FF] h-1 mt-2 rounded-full {headerInView?'w-24':'w-0'} duration-[3000ms] ease-in-out group-hover:w-52 duration-500"></div>
+        <a class="text-white text-4xl font-black tracking-widest" rel="noopener noreferrer" target="_blank" href="https://github.com/realTristan?tab=repositories">PROJECTS</a>
+        <div class="bg-[#1014FF] h-1 mt-2 rounded-full {headerInView?'w-24':'w-0'} duration-[3000ms] ease-in-out group-hover:w-52"></div>
     </div>
     <div class="mt-16">
         {#each projectsInView as s}
