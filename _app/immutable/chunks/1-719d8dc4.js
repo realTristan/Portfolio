2c72a8aae1c21c9fc859472a2f55f09edba91ff2
Tristan Simpson <heytristaann@gmail.com>
2022-10-14 22:28:48 -0400

    Updates

--- _app/immutable/chunks/1-719d8dc4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-3ec8431a.js";export{t as component};
--- _app/immutable/chunks/3-fd13d915.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-8faa1ff6.js";import{default as t}from"../components/pages/projects/_page.svelte-c3f44862.js";export{t as component,r as shared};
