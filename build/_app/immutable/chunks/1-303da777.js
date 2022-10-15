1b329d93d3a8af7ba3f174d2f49d90b633c4d2f0
tristan <heytristaann@gmail.com>
2022-10-14 22:28:50 -0400

    new build

---
+++ build/_app/immutable/chunks/1-303da777.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-0192aec3.js";export{t as component};
---
+++ build/_app/immutable/chunks/3-b2b5d211.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-8faa1ff6.js";import{default as t}from"../components/pages/projects/_page.svelte-3231a9d0.js";export{t as component,r as shared};
