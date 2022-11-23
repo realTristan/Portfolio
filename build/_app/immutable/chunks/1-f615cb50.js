6ebf9a6adbb188742d9f2fe179a41dac224c1b40
tristan <heytristaann@gmail.com>
2022-11-23 13:45:53 -0500

    changed fonts

--- build/_app/immutable/chunks/1-f615cb50.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-aef7005a.js";export{t as component};
--- src/routes/components/SubHeader.svelte
+++ src/routes/components/SubHeader.svelte
@@ -5,6 +5,6 @@
     export let outfade;
 </script>
 
-<h2 class="font-black text-4xl lg:text-6xl italic text-white tracking-widest mt-10" style="z-index: -1;" in:fade={infade} out:fade={outfade}>
-    <mark style="background: none;" class="text-[#38ffff]">+</mark>{title}
+<h2 class="font-black text-4xl lg:text-6xl italic text-white tracking-widest mt-10 font-sans" style="z-index: -1;" in:fade={infade} out:fade={outfade}>
+    <mark style="background: none;" class="text-[#38ffff] mr-2">+</mark>{title}
 </h2>
