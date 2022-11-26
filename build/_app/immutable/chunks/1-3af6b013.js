34f57a170e6d3de5b5a192aa8a86c7cd7c3df051
tristan <heytristaann@gmail.com>
2022-11-26 16:00:26 -0500

    fixed wavy sphere on resize

--- build/_app/immutable/chunks/1-3af6b013.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-537e79ba.js";export{t as component};
---
+++ build/_app/immutable/chunks/1-71ea4aed.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-40405a4b.js";export{t as component};
