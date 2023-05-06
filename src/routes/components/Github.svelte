6c1b020b71a877a7ef19427663393534584745d0
tristan <heytristaann@gmail.com>
2023-05-06 13:39:42 -0400

    cleaned up

--- src/routes/components/Github.svelte
+++ src/routes/components/Github.svelte
@@ -32,11 +32,12 @@
     }
   }}
 >
-  <a href="https://github.com/realTristan" class="text-white text-4xl font-black tracking-widest">GITHUB</a>
+  <a href="https://github.com/realTristan" rel="noopener noreferrer" target="_blank" class="text-white text-4xl font-black tracking-widest">GITHUB</a>
   <div class="bg-[#38ffff] h-1 mt-2 rounded-full {headerInView ? 'w-24': 'w-0'} duration-[2000ms] ease-in-out"/>
   <div class="flex mt-6 mb-12 ml-4 flex-wrap w-[23rem] xl:w-[30rem] 2xl:w-[40rem]">
     <h2 class="text-gray-300 mb-10 mr-1 text-left text-xl leading-relaxed xl:text-2xl tracking-widest xl:leading-loose">
-      My <a href="https://github.com/realTristan" class="text-blue-600 hover:underline duration-200">Github</a> is home to over 30 projects, each built with various
+      My <a href="https://github.com/realTristan" rel="noopener noreferrer" target="_blank" class="text-[#38ffff] hover:underline duration-200">Github</a> 
+      is home to over 30 projects, each built with various
       languages, stacks, and services. I'm most proficient in developing
       applications with Python, Go, Rust, and Typescript.
     </h2>
